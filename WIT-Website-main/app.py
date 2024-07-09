b3f146995d0aab51f12397fb5cbf9088a629321f
Cindy <cngu5238@uni.sydney.edu.au>
2024-07-09 18:28:18 +1000

    file cleanup and scaffold setup

---
+++ WIT-Website-main/app.py
@@ -0,0 +1,55 @@
+
+from flask import Flask, render_template, request, abort, url_for
+from flask_socketio import SocketIO
+import secrets
+
+
+app = Flask(__name__)
+
+# secret key used to sign the session cookie
+app.config['SECRET_KEY'] = secrets.token_hex()
+socketio = SocketIO(app)
+
+# don't remove this!!
+# import socket_routes
+
+# home page
+@app.route("/")
+def index():
+    return render_template("home.jinja")
+
+# About Us page
+@app.route("/aboutus")
+def aboutus():
+    return render_template("aboutus.jinja")
+
+# Events page
+@app.route("/events")
+def events():
+    return render_template("events.jinja")
+
+# Sponsors page
+@app.route("/sponsors")
+def sponsors():
+    return render_template("sponsors.jinja")
+
+# Publications page
+@app.route("/publications")
+def publications():
+    return render_template("publications.jinja")
+
+# Contact Us page
+@app.route("/contactus")
+def contactus():
+    return render_template("contactus.jinja")
+
+
+# handler when a "404" error happens
+@app.errorhandler(404)
+def page_not_found(_):
+    return render_template('404.jinja'), 404
+
+
+if __name__ == '__main__':
+    socketio.run(app)
+    
