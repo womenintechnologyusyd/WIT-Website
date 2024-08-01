d652360449abb7282edb1a9bd1093bcdab5ffbbf
Cindy <cngu5238@uni.sydney.edu.au>
2024-08-01 23:23:37 +1000

    working dropdown menu

--- WIT-Website-main/static/js/libs/main.js
+++ WIT-Website-main/static/js/libs/main.js
@@ -25,3 +25,33 @@ function changeSlide(n) {
     imgs[currentImg].style.opacity = 1;
     dots[currentImg].className += ' activebanner';
 }
+
+
+// NAVBAR
+// function collapseNav() {
+//     console.log("HERE")
+//     var x = document.getElementById("page-link-wrapper");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+// }
+
+function collapseNav() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
