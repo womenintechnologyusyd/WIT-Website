d98d65ee5596b628a2d4adeb1af2fff0e41f44d2
c-ngu <cindy12nguyen.school@gmail.com>
2025-03-15 23:56:49 +1100

    trying to fix missing php file 2

--- templates/test2.php
+++ templates/test2.php
@@ -1,9 +1,12 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 
 if(isset($_POST['submit'])) {
 
-	header("Location: http://www.witusyd.com");
+	header("Location: http://www.witusyd.com/");
     exit();
 
 	// //Email enquiry will be sent to
