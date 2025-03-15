a2535a162ea4a7fa41f09ddb6eea2f79d01e794e
c-ngu <cindy12nguyen.school@gmail.com>
2025-03-15 22:47:19 +1100

    changing page redirection

--- php-temp/test2.php
+++ php-temp/test2.php
@@ -1,37 +1,41 @@
 <?php
 
+
 if(isset($_POST['submit'])) {
-	//Email enquiry will be sent to
-	$mailTo = "president@witusyd.com";
 
-	//User's Input
-	$firstName = $_POST['first_name'];
-	$lastName = $_POST['last_name'];
-	$email = $_POST['email'];
-	$subject = $_POST['subject'];
-	$message = $_POST['message'];
+	header(Location: "contactus.jinja?emailsent");
 
-	//Checking for valid input
-	if (!preg_match("/^[a-zA-Z-' ]*$/",$firstName)) {
-		$nameErr = "Only letters and white space allowed";
-		echo '<script type="text/javascript">alert($nameErr);</script>';
-	}
+	// //Email enquiry will be sent to
+	// $mailTo = "president@witusyd.com";
 
-	//Email Formatting
-	$headers = "From:" .$email;
-	$txt = "You have recieved a message from " .$firstName .$lastName. "\n\n\n".$message;
+	// //User's Input
+	// $firstName = $_POST['first_name'];
+	// $lastName = $_POST['last_name'];
+	// $email = $_POST['email'];
+	// $subject = $_POST['subject'];
+	// $message = $_POST['message'];
 
-	$result = mail($mailTo, $subject, $txt, $headers);
+	// //Checking for valid input
+	// if (!preg_match("/^[a-zA-Z-' ]*$/",$firstName)) {
+	// 	$nameErr = "Only letters and white space allowed";
+	// 	echo '<script type="text/javascript">alert($nameErr);</script>';
+	// }
 
-	// if ($result) {
-    //     echo '<script type="text/javascript">alert("Your message was sent!");</script>';
-    //     echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+	// //Email Formatting
+	// $headers = "From:" .$email;
+	// $txt = "You have recieved a message from " .$firstName .$lastName. "\n\n\n".$message;
 
-	// } else {
-    //     echo '<script type="text/javascript">alert("Oops! Your message wasn’t sent. Try again later.");</script>';
-    //     echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
-    // }
-	header(Location: "contactus.jinja?emailsent")
+	// $result = mail($mailTo, $subject, $txt, $headers);
+
+	// // if ($result) {
+    // //     echo '<script type="text/javascript">alert("Your message was sent!");</script>';
+    // //     echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+
+	// // } else {
+    // //     echo '<script type="text/javascript">alert("Oops! Your message wasn’t sent. Try again later.");</script>';
+    // //     echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+    // // }
+	// header(Location: "contactus.jinja?emailsent");
 
 }
 
--- static/js/libs/main.js
+++ static/js/libs/main.js
@@ -1,50 +1,50 @@
 
-//SLIDESHOW SCRIPT
+// //SLIDESHOW SCRIPT
 
-var imgs = document.querySelectorAll('.slider img');
-var dots = document.querySelectorAll('.dot');
-var currentImg = 0; // index of the first image 
-const interval = 4000; // duration(speed) of the slide
+// var imgs = document.querySelectorAll('.slider img');
+// var dots = document.querySelectorAll('.dot');
+// var currentImg = 0; // index of the first image 
+// const interval = 4000; // duration(speed) of the slide
 
-var timer = setInterval(changeSlide, interval);
+// var timer = setInterval(changeSlide, interval);
 
-function changeSlide(n) {
-    for (var i = 0; i < imgs.length; i++) { // reset
-        imgs[i].style.opacity = 0;
-        dots[i].className = dots[i].className.replace(' activebanner', '');
-    }
+// function changeSlide(n) {
+//     for (var i = 0; i < imgs.length; i++) { // reset
+//         imgs[i].style.opacity = 0;
+//         dots[i].className = dots[i].className.replace(' activebanner', '');
+//     }
 
-    currentImg = (currentImg + 1) % imgs.length; // update the index number
+//     currentImg = (currentImg + 1) % imgs.length; // update the index number
 
-    if (n != undefined) {
-        clearInterval(timer);
-        timer = setInterval(changeSlide, interval);
-        currentImg = n;
-    }
+//     if (n != undefined) {
+//         clearInterval(timer);
+//         timer = setInterval(changeSlide, interval);
+//         currentImg = n;
+//     }
 
-    imgs[currentImg].style.opacity = 1;
-    dots[currentImg].className += ' activebanner';
-}
+//     imgs[currentImg].style.opacity = 1;
+//     dots[currentImg].className += ' activebanner';
+// }
 
 
-// NAVBAR
+// // NAVBAR
 
-function collapseNav() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+// function collapseNav() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
   
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
     
-}
+// }
 
