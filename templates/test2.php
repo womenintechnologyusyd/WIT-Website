5a0af3ec7202dba22429f97ad2f6bda3b770af79
c-ngu <cindy12nguyen.school@gmail.com>
2025-03-15 23:23:35 +1100

    test 3

--- templates/test2.php
+++ templates/test2.php
@@ -1,41 +1,44 @@
 <?php
 
+echo '<script type="text/javascript">alert("Inside PHP!");</script>';
+
 
 if(isset($_POST['submit'])) {
 
-	print("Accepted\n\n");
+	echo '<script type="text/javascript">alert("Your message was sent!");</script>';
+	header(Location: "home.jinja");
 
-	//Email enquiry will be sent to
-	$mailTo = "president@witusyd.com";
+	// //Email enquiry will be sent to
+	// $mailTo = "president@witusyd.com";
 
-	//User's Input
-	$firstName = $_POST['first_name'];
-	$lastName = $_POST['last_name'];
-	$email = $_POST['email'];
-	$subject = $_POST['subject'];
-	$message = $_POST['message'];
+	// //User's Input
+	// $firstName = $_POST['first_name'];
+	// $lastName = $_POST['last_name'];
+	// $email = $_POST['email'];
+	// $subject = $_POST['subject'];
+	// $message = $_POST['message'];
 
-	//Checking for valid input
-	if (!preg_match("/^[a-zA-Z-' ]*$/",$firstName)) {
-		$nameErr = "Only letters and white space allowed";
-		echo '<script type="text/javascript">alert($nameErr);</script>';
-	}
+	// //Checking for valid input
+	// if (!preg_match("/^[a-zA-Z-' ]*$/",$firstName)) {
+	// 	$nameErr = "Only letters and white space allowed";
+	// 	echo '<script type="text/javascript">alert($nameErr);</script>';
+	// }
 
-	//Email Formatting
-	$headers = "From:" .$email;
-	$txt = "You have recieved a message from " .$firstName .$lastName. "\n\n\n".$message;
+	// //Email Formatting
+	// $headers = "From:" .$email;
+	// $txt = "You have recieved a message from " .$firstName .$lastName. "\n\n\n".$message;
 
-	$result = mail($mailTo, $subject, $txt, $headers);
+	// $result = mail($mailTo, $subject, $txt, $headers);
 
-	// if ($result) {
-    //     echo '<script type="text/javascript">alert("Your message was sent!");</script>';
-    //     echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+	// // if ($result) {
+    // //     echo '<script type="text/javascript">alert("Your message was sent!");</script>';
+    // //     echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
 
-	// } else {
-    //     echo '<script type="text/javascript">alert("Oops! Your message wasn’t sent. Try again later.");</script>';
-    //     echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
-    // }
-	header(Location: "contactus.jinja?emailsent");
+	// // } else {
+    // //     echo '<script type="text/javascript">alert("Oops! Your message wasn’t sent. Try again later.");</script>';
+    // //     echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+    // // }
+	// header(Location: "contactus.jinja?emailsent");
 
 }
 
